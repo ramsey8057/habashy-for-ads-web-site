fabf10c0a8598b95e71ec473f9084cc23c0fcaf1
Ahmed Ramsey <54491254+ramsey8057@users.noreply.github.com>
2020-01-24 21:08:37 +0200

    Created the basic structure

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang"en">
+    <head>
+        <!-- meta tags -->
+        <meta charset="UTF-8">
+        <meta name="description" content="Habashy for advertising">
+        <meta name="keywords" content="habashy, ads, advertising, photo, sessions, advertise, printage">
+        <meta name="author" content="Ahmed Ramsey">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <!-- bootstrap styles CDN -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <!-- styles link -->
+        <link rel="stylesheet" href="assets/styles/style.css">
+        <link rel="stylesheet" href="assets/styles/navbar.css">
+        <link rel="stylesheet" href="assets/styles/particles.css">
+        <!-- title image -->
+        <link rel="icon" href="assets/images/habashy.jpg">
+        <!-- title tag -->
+        <title>Habashy | Home</title>
+    </head>
+    <body dir="en">
+        <!-- particles -->
+        <div id="particles-js">
+            <!-- navbar -->    
+            <nav>
+                <div class="logo">
+                    <h1>Habashy</h1>
+                </div>
+                <ul class="nav-links">
+                    <li><a hred="#">Home</a></li>
+                    <li><a hred="/about.php">About</a></li>
+                    <li><a hred="/last_works.php">Last Works</a></li>
+                </ul>
+                <div class="burger">
+                    <div class="line1"></div>
+                    <div class="line2"></div>
+                    <div class="line3"></div>
+                </div>
+            </nav>
+        </div>
+        <!-- jquery -->
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <!-- bootstrap scripts CDN -->
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        <!-- particles scripts -->
+        <script src="https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js"></script>
+        <script>
+            particlesJS.load("particles-js", "assets/json/particles.json", function() {
+                console.log("Particles js loaded successfully");
+            });
+        </script>
+        <!-- scripts -->
+        <script src="assets/scripts/global.js"></script>
+        <script src="assets/scripts/navbar.js"></script>
+    </body>
+</html>
