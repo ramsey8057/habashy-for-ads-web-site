b8c902a2b8c139144c8e350b740dc2834c0d3ff0
Ahmed Ramsey <54491254+ramsey8057@users.noreply.github.com>
2020-02-21 17:45:45 +0200

    Update last_works.php

--- last_works.php
+++ last_works.php
@@ -51,7 +51,7 @@
                     ';
                 }
                 ?>
-                <div class="col-md-12 col-sm-12 col-lg-12 col-xs-12" style="background: #333332;">
+                <!-- <div class="col-md-12 col-sm-12 col-lg-12 col-xs-12" style="background: #333332;">
                     <div class="col-md-4 col-sm-12 col-lg-4 col-xs-12">
                         <div class="row">
                             <div class="col-md-12 col-sm-12 col-lg-12 col-xs-12">
@@ -101,7 +101,7 @@
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> -->
             </div>
         </div>
         <!-- jquery -->
